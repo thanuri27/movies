6117d2b75b0e8d9d749b41405c65868ca3ca53be
thanuri27 <tthamaradini@gmail.com>
2024-08-10 13:25:42 +0530

    moview

---
+++ QuizPage/QuizQuestions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "In the movie 'Zootopia', what type of animal is the main character, Judy Hopps? ",
+    answer: "Rabbit",
+    options: [
+      "Rabbit",
+      "Monkey",
+      "Squirrel",
+      "Cat"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Who portraits the main character of the movie 'Mission Impossible'?",
+    answer: "Tom Cruise",
+    options: [
+      "Esai Morales",
+      "Tom Cruise",
+      "Simon Pegg",
+      "Johnny Depp"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Who directed the 'Avatar' Movie?",
+    answer: "James Cameron",
+    options: [
+      "Tim Burton",
+      "James Cameron",
+      "Ridley Scott",
+      "Peter Jackson"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Who plays the role of 'Jack Sparrow'?",
+    answer: "Johnny Depp",
+    options: [
+      "Tom Cruise",
+      "Leonardo dicaprio",
+      "Tom Holland",
+      "Johnny Depp"
+    ]
+  },
+  {
+    numb: 5,
+    question: "In the movie 'Frozen', what is the name of the snowman character?",
+    answer: "Olaf",
+    options: [
+      "Genie",
+      "Hei hei",
+      "Olaf",
+      "Pua"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the house of Harry Potter?",
+    answer: "Gryffindor",
+    options: [
+      "Gryffindor",
+      "Ravenclaw",
+      "Hufflepuff",
+      "Slytherin"
+    ]
+  },
+  {
+    numb: 7,
+    question: "In which movie can you find T.Rex dinosaur?",
+    answer: "Jurrasic Park",
+    options: [
+      "Pirates of the caribbean",
+      "Fast and Furious",
+      "Avatar",
+      "Jurrasic Park"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How many Golden Tickets are waiting to be found in Charlie and the Chocolate Factory?",
+    answer: "5",
+    options: [
+      "10",
+      "4",
+      "5",
+      "3"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which movie has Will Smith played in?",
+    answer: "Aladdin",
+    options: [
+      "Aladdin",
+      "Titanic",
+      "Frozen",
+      "Aquaman"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who played Edward in the Twilight film?",
+    answer: "Robert Pattinson",
+    options: [
+      "Taylor Lautner",
+      "Robert Pattinson",
+      "Andrew Garfield",
+      "Ryan Gosling"
+    ]
+  },
+];
